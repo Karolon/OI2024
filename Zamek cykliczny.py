d773648493ffd6d57fd830dd6120f914e1c7afdf
Karolon <109922689+Karolon@users.noreply.github.com>
2024-11-06 21:14:14 +0100

    zamek cykliczny ogarnięty
    i próba robotów

--- Zamek cykliczny.py
+++ Zamek cykliczny.py
@@ -1,24 +1,33 @@
+import sys
 n = input()
-
+sys.set_int_max_str_digits(0)
 amount = 0
 while True:
-  if not('2' in n or '3' in n or '4' in n or '5' in n or '6' in n or '7' in n or '8' in n or '9' in n):
-    if n.count('1') == 1:
-      break
-  if n[-1] == '0':
+  '''  if not('2' in n or '3' in n or '4' in n or '5' in n or '6' in n or '7' in n or '8' in n or '9' in n):
+      if n.count('1') == 1:
+        break'''
+  if n[-1] == '9' or n[-1] == '0':
     n+=n[0]
     n = n[1:]
     n=str(int(n))
     amount+=1
-  else:
-    while n[-1] != '0':
+  elif n != '1':
+    while n[-1] != '9':
       n = str(int(n)+1)
-    amount+=1
-  print(n)
-if n != '1':
+      amount+=1
+  if not('2' in n or '3' in n or '4' in n or '5' in n or '6' in n or '7' in n or '8' in n or '1' in n or '0' in n) or n == '1':
+    break
+
+  
+if len(n) != 1:
+  n = str(int(n)+1)
+  amount+=1
+  n+=n[0]
+  n = n[1:]
   n=str(int(n))
   amount+=1
-  
-
+print(n)
 
 print(amount)
+while True:
+  pass
--- walki.py
+++ walki.py
@@ -2,6 +2,7 @@ n = int(input())
 robots = []
 s_max =[0,0]
 z_max = [0,0]
+sz = 0
 
 for i in range(n):
   s,z = input().split()
@@ -11,7 +12,25 @@ for i in range(n):
     s_max = [s,z]
   if z_max[1] > z:
     z_max = [s,z]
+  if s == z:
+    sz +=1
     
-if s_max == z_max:
+if robots.index(s_max) == robots.index(z_max):
   print('NIE')
+elif len(robots) % 2 == sz % 2:  # jeżeli jest nieparzysta ilość robotów  i nieparzysta ilość robótów u kturych s == z to NIE (jeżli obie są ilości są parzyste to można usunąć jednego robota z s == z i zmienić je na nieparzyste)
+  print('NIE')
+
+
+
+#jeżeli jest robot s==z to zawsze da się doprowadzić do tego żeby było jeden robot gdzie s == z
+#\tak wtedy gdy ilość jest parzysta i nigdzie z != s oraz w wszystkich przypadkach które są w stanie do tego doprowadzić
+#\jeżeli jest pażyste to znajdzie się tak możliwość(chyba że istnieje s == z)
+#co jeżeli liczba robotów jest nieparzysta gdzie nie ma s==z?
+#-
+
+
+
+
+
+  
 #jeżeli liczba robotów które mogą wygrać siłą == liczba robotów które mogą wygrać zręczniścią
